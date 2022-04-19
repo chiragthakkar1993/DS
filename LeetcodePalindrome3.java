f034826c4b6927432a56e19a56d921134f8b2cc1
Chirag Thakkar <chirag.thakkar1993@gmail.com>
2022-04-19 22:10:35 +0530

    Create LeetcodePalindrome3.java

---
+++ LeetcodePalindrome3.java
@@ -0,0 +1,26 @@
+class LeetcodePalindrome3 {
+    public boolean checkPalindrome(String s, int i, int j){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;   
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public boolean validPalindrome(String s) {
+        int i=0,j=s.length()-1;
+        int count = 0;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return (checkPalindrome(s,i+1,j) || checkPalindrome(s,i,j-1));
+                
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
